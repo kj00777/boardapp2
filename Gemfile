b8af6410ff54477dd48ba874ea2f0200e4b9a676
vagrant <vagrant@localhost.localdomain>
2019-04-14 13:41:31 +0000

    i18n,timezon,rails g file settings

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'jbuilder', '~> 2.5'
 gem "rubocop-rails_config"
 gem 'rubocop-performance'
 
+gem 'rails-i18n', '~> 5.1'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,15 @@ Bundler.require(*Rails.groups)
 
 module Boardapp
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
