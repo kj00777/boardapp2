4f8fa17953f964f9d49f610fbddf801f8b0b405f
vagrant <vagrant@localhost.localdomain>
2019-04-30 02:04:08 +0000

    lesson6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  protected
+
   def not_authenticated
     redirect_to "/login"
   end
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,14 +7,10 @@ class UserSessionsController < GeneralController
     end
 
     def create
-        puts "aaaaaaaaaaaaaaaa"
+        
         @user = login(params[:email], params[:password])
-        puts params[:email]
-        puts params[:password]
-        puts @user
-        puts "bbbbbbb"
+        
         if @user = login(params[:email], params[:password])
-            puts "aaaaaa"
             redirect_back_or_to(root_path, notice: 'Login successful')
           else
             flash.now[:alert] = 'Login failed'
