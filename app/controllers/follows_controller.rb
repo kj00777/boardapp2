678807ebaf44d49137ea57014fa1d6ccb2582645
vagrant <vagrant@localhost.localdomain>
2019-05-04 15:35:52 +0000

    lesson 14

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,20 +2,22 @@ class FollowsController < ApplicationController
 
     def create
         follow = Follow.create(user_id: current_user.id, board_id: params[:board_id])
+        @board = Board.find_by(id: params[:board_id])
         if follow
-            redirect_back(fallback_location: root_path)
+            # redirect_back(fallback_location: root_path)
         else
-            redirect_back(fallback_location: root_path)
+            # redirect_back(fallback_location: root_path)
         end
     end
 
 
     def destroy
         follow = Follow.find_by(user_id: current_user.id, board_id: params[:board_id])
+        @board = Board.find_by(id: params[:board_id])
         if follow.destroy  
-            redirect_back(fallback_location: root_path)
+            # redirect_back(fallback_location: root_path)
         else
-            redirect_back(fallback_location: root_path)
+            # redirect_back(fallback_location: root_path)
         end
     end
 
